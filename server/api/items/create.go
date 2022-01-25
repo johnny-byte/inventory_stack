0b619a8689a589f53e14147a71c35ae646cbbc07
maxnemoy <maxnemoy@gmail.com>
2022-01-25 11:05:30 +0800

    revert

--- server/api/items/create.go
+++ server/api/items/create.go
@@ -3,9 +3,14 @@ package items
 import (
 	"migration_server/db/models"
 	"net/http"
+	"time"
+
 	// "time"
 
 	"github.com/go-pg/pg/v10"
+	"github.com/gookit/validate"
+	uuid "github.com/satori/go.uuid"
+
 	// "github.com/gookit/validate"
 	"github.com/labstack/echo/v4"
 	// uuid "github.com/satori/go.uuid"
@@ -15,31 +20,30 @@ import (
 func Create(conn *pg.DB) func(ctx echo.Context) error {
 	return func(ctx echo.Context) error {
 		item := &models.Item{}
-		// defaultPlace := &models.Place{}
-		// defaultPlace, _ = defaultPlace.GetDefault(conn)
-		// fmt.Println(defaultPlace)
+		defaultPlace := &models.Place{}
+		defaultPlace, _ = defaultPlace.GetDefault(conn)
 
-		// if err := ctx.Bind(item); err != nil {
-		// 	return ctx.JSON(http.StatusBadRequest, struct{ Error string }{err.Error()})
-		// }
+		if err := ctx.Bind(item); err != nil {
+			return ctx.JSON(http.StatusBadRequest, struct{ Error string }{err.Error()})
+		}
 
-		// v := validate.Struct(item)
-		// if v.Validate() {
-		// 	item.CreateAt = time.Now()
-		// 	item.UpgradeAt = time.Now()
-		// 	//item.RootPlaceUUID = defaultPlace.UUID
-		// 	var err error
-		// 	item.UUID = uuid.Must(uuid.NewV4(), err).String()
-		// 	if err != nil {
-		// 		return err
-		// 	}
-		// 	// if err := item.CreateItem(conn); err != nil {
-		// 	// 	return ctx.JSON(http.StatusBadRequest,
-		// 	// 		struct{ Error string }{err.Error()})
-		// 	// }
-		// } else {
-		// 	return ctx.JSON(http.StatusBadRequest, v.Errors.All())
-		// }
+		v := validate.Struct(item)
+		if v.Validate() {
+			item.CreateAt = time.Now()
+			item.UpgradeAt = time.Now()
+			item.RootPlaceUUID = defaultPlace.UUID
+			var err error
+			item.UUID = uuid.Must(uuid.NewV4(), err).String()
+			if err != nil {
+				return err
+			}
+			if err := item.CreateItem(conn); err != nil {
+				return ctx.JSON(http.StatusBadRequest,
+					struct{ Error string }{err.Error()})
+			}
+		} else {
+			return ctx.JSON(http.StatusBadRequest, v.Errors.All())
+		}
 
 		return ctx.JSON(http.StatusCreated, item)
 	}
